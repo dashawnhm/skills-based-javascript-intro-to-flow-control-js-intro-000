c1a8dc77ee84020c51abeb8353bb35f8a2fe6bd4
Dashawn <dashawnhm@gmail.com>
2020-10-24 05:41:59 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,7 +18,9 @@ else if(age<=12) {return `You are a kid`}
 else if(age>=19){return `You are a grownup`}
 }
 
-function ternaryTeenager(age) {
+function ternaryTeenager(age) {return 13<=age && age<=19?
+`You are a teenager`:
+`You are not a teenager`
 }
 
 function switchAge(age) {
